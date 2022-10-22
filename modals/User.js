2bd37faac1e70ef71d538fbb31216018d5a662bf
Ahzam Akhtar <ahzam8186@gmail.com>
2022-10-23 02:43:24 +0530

    Update

---
+++ modals/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name : {type:String , required:true},
+    email : {type :String , required:true , unique :true},
+    password:{type:String , requied:true}
+},{timestamps:true})
+mongoose.models = {}
+export default mongoose.model("User",UserSchema)
