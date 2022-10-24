7f58ba47d35b5a6f673dce4dcf71545be55e17f8
Ahzam Akhtar <ahzam8186@gmail.com>
2022-10-24 23:55:44 +0530

    yOOO

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,104 +1,51 @@
-import React from 'react'
-import Link from 'next/link'
-const tshirts = () => {
+import React from "react";
+import Link from "next/link";
+import Product from "../modals/Product"
+import mongoose from "mongoose";
+
+const tshirts = ({products}) => {
   return (
     <div>
       <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      <Link href={"product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p class="mt-1">$16.00</p>
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            {products.map((item)=>{
+            return <Link key={item._id} href={`product/${item.slug}`}>
+              <div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+                <a class="block relative rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block"
+                    src={item.img}
+                  />
+                </a>
+                <div class="mt-4 text-center md:text-left">
+                  <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 class="text-gray-900 title-font text-lg font-medium">
+                    {item.title}
+                  </h2>
+                  <p class="mt-1">{"₹"+item.price}</p>
+                </div>
+              </div>
+            </Link>})}
+          </div>
         </div>
-      </div>
-      </Link> 
-      <Link href={"product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p class="mt-1">$21.15</p>
-        </div>
-      </div>
-      </Link> 
-      <Link href={"product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1">$12.00</p>
-        </div>
-      </div>
-      </Link> 
-      <Link href={"product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1">$18.40</p>
-        </div>
-      </div>
-      </Link> 
-      <Link href={"product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p class="mt-1">$16.00</p>
-        </div>
-      </div>
-      </Link> 
-      <Link href={"/product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p class="mt-1">$21.15</p>
-        </div>
-      </div>
-      </Link>
-      <Link href={"/product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1">$12.00</p>
-        </div>
-      </div>
-      </Link>
-      <Link href={"/product/wear-the-code"}><div class="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
-        <a class="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" class="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/41JEBpNuz5L._UY741_.jpg"/>
-        </a>
-        <div class="mt-4 text-center md:text-left">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1">$18.40</p>
-        </div>
-      </div>
-      </Link>
+      </section>
     </div>
-  </div>
-</section>
-    </div>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+    
+  }
+  let products = await Product.find();
+  return {
+    props: {products : JSON.parse(JSON.stringify(products))},
+  };
 }
 
-export default tshirts
+export default tshirts;
